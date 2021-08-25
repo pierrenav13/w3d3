ebfae56d25e0566c2c6eb31795aa010710564793
Zeki <zckural@gmail.com>
2021-08-25 14:24:26 -0400

    subsets

--- recursion.rb
+++ recursion.rb
@@ -1,4 +1,4 @@
-require 'byebug'
+require "byebug"
 
 def range(n_s, n_e)
   return [n_s] if n_e == n_s + 1
@@ -42,13 +42,13 @@ end
 class Array
   def deep_dup
     arr = Array.new
-    old_arr = self.map {|ele| ele}
+    old_arr = self.map { |ele| ele }
     old_arr.each do |ele|
-        unless ele.is_a?(Array)
-            arr += [ele]
-        else
-            arr += ele.deep_dup
-        end
+      unless ele.is_a?(Array)
+        arr += [ele]
+      else
+        arr += ele.deep_dup
+      end
     end
   end
 end
@@ -59,9 +59,9 @@ end
 # p b.object_id
 
 def fibonacci(n)
-    return 1 if n == 1
-    return 1 if n == 2
-    fibonacci(n-1) + fibonacci(n-2)
+  return 1 if n == 1
+  return 1 if n == 2
+  fibonacci(n - 1) + fibonacci(n - 2)
 end
 
 # p fibonacci(3)
@@ -69,24 +69,22 @@ end
 # p fibonacci(5)
 # p fibonacci(6)
 
-
 def bsearch(array, target)
-    middle_idx = array.length / 2
-    middle = array[middle_idx]
+  middle_idx = array.length / 2
+  middle = array[middle_idx]
 
-    
-    return middle_idx if target == middle
-    return nil if array.length == 1
-    if target < middle
-        return bsearch(array[0...middle_idx], target)
-    elsif target > middle
-        b_searched = bsearch(array[middle_idx..-1], target)
-        if b_searched == nil
-            return nil
-        else
-            return middle_idx + b_searched
-        end
+  return middle_idx if target == middle
+  return nil if array.length == 1
+  if target < middle
+    return bsearch(array[0...middle_idx], target)
+  elsif target > middle
+    b_searched = bsearch(array[middle_idx..-1], target)
+    if b_searched == nil
+      return nil
+    else
+      return middle_idx + b_searched
     end
+  end
 end
 
 # p bsearch([1, 2, 3], 1) # => 0
@@ -97,37 +95,38 @@ end
 # p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
 # p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
 
-
 def merge_sort(array)
-    return [] if array.length == 0
-    return array if array.length == 1
-    middle_idx = array.length / 2
-
+  return [] if array.length == 0
+  return array if array.length == 1
+  middle_idx = array.length / 2
+  arr_1 = array[0...middle_idx]
+  arr_2 = array[middle_idx..-1]
+  merge(merge_sort(arr_1), merge_sort(arr_2))
 end
 
 def merge(ele_1, ele_2)
-    merged = ele_1 + ele_2
+  merged = ele_1 + ele_2
+  merged.each_with_index do |el1, i1|
+    merged.each_with_index do |el2, i2|
+      if i2 > i1
+        if el1 > el2
+          merged[i1], merged[i2] = merged[i2], merged[i1]
+        end
+      end
+    end
+  end
+end
 
-    merged.each_with_index do |el1, i1|
-        merged.each_with_index do |el2, i2|
-            if i2 > i1
-                if el1 > el2
-                    merged[i1], merged[i2] = merged[i2], merged[i1]
-                end
-            end
+
+def subsets(array)
+    return [[]] if array.length == 0
+    return [[],array] if array.length == 1
+    debugger
+    subs = subsets(array[0...-1])
+    subs.each do |ele|
+        unless ele.include?(array[-1])
+            subs << ele+[array[-1]]
         end
     end
-
-
-    # ele_1.each do |el1|
-    #     ele_2.each do |el2|
-    #         if el2 < el1
-    #             merged << el2
-    #             #break
-    #         else
-    #             merged << el1
-    #         end
-    #     end
-    # end
-    merged
+    subs
 end
